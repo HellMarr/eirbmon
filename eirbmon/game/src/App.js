256dfab601a6f59882c1fd92a35a4165406010da
nadia-hilmi <hilmi.nadia13@hotmail.com>
2022-05-16 15:12:47 +0200

    send wallet to game

--- eirbmon/game/src/App.js
+++ eirbmon/game/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Unity, { UnityContext } from "react-unity-webgl";
 import detectEthereumProvider from '@metamask/detect-provider';
-import React from 'react';
+import React, { useState } from 'react';
 import Web3 from "web3/dist/web3.min.js";
 const getBalance = require("./blockchain").getBalance;
 
@@ -16,70 +16,81 @@ const unityContext = new UnityContext({
 });
 
 const getAddr = async (provider) => {
-  const addr = await provider.request({method: 'eth_requestAccounts'});
+  const addr = await provider.request({ method: 'eth_requestAccounts' });
   return addr[0];
 }
 
-function render(authorized){
-  if(authorized==="authorized"){
-    return <Unity 
-      unityContext={unityContext} 
-      style={{
-        width: "1000px",
-        height: "750px",
-        border: "2px solid black",
-        background: "white",
-      }}
-    />
-  }else if(authorized==="not authorized"){
+function render(authorized,addr) {
+  if (authorized === "authorized") {
+    return (
+      <div>
+        <Unity
+          unityContext={unityContext}
+          style={{
+            width: "1000px",
+            height: "750px",
+            border: "2px solid black",
+            background: "white",
+          }}
+        />
+        {unityContext.send("PlayerController", "setWallet", addr)} 
+      </div>
+
+    )
+
+  } else if (authorized === "not authorized") {
     return <div>
-            <h1>You must first buy an NFT before accessing the game</h1>
-            <form action="http://localhost:8080/?#/marketplace">
-              <button type="submit" style={{marginBottom:'40px',padding:'10px 20px 10px 20px',fontSize:'32px',background:'linear-gradient(-90deg, #FD992D 0%, #FFBF49 100%)',borderRadius:'1em', fontWeight:'bold'}}>Go to marketplace</button>
-            </form>
-          </div>
-  }else if(authorized==="loading"){
+      <h1>You must first buy an NFT before accessing the game</h1>
+      <form action="http://localhost:8080/?#/marketplace">
+        <button type="submit" style={{ marginBottom: '40px', padding: '10px 20px 10px 20px', fontSize: '32px', background: 'linear-gradient(-90deg, #FD992D 0%, #FFBF49 100%)', borderRadius: '1em', fontWeight: 'bold' }}>Go to marketplace</button>
+      </form>
+    </div>
+  } else if (authorized === "loading") {
     return <h1>Loading ...</h1>
-  }else{
+  } else {
     return <div>
-            <h1>Please install metamask before accessing the game</h1>
-            <form action="https://metamask.io/download/">
-              <button type="submit" style={{marginBottom:'40px',padding:'10px 20px 10px 20px',fontSize:'32px',background:'linear-gradient(-90deg, #FD992D 0%, #FFBF49 100%)',borderRadius:'1em', fontWeight:'bold'}}>
-                Download Metamask
-                <img src="./metamask.png" style={{width: "30px", marginLeft: "15px"}}></img>
-              </button>
-            </form>
-          </div>
+      <h1>Please install metamask before accessing the game</h1>
+      <form action="https://metamask.io/download/">
+        <button type="submit" style={{ marginBottom: '40px', padding: '10px 20px 10px 20px', fontSize: '32px', background: 'linear-gradient(-90deg, #FD992D 0%, #FFBF49 100%)', borderRadius: '1em', fontWeight: 'bold' }}>
+          Download Metamask
+          <img src="./metamask.png" style={{ width: "30px", marginLeft: "15px" }}></img>
+        </button>
+      </form>
+    </div>
   }
 }
 
 function App() {
   const [authorized, setAuthorized] = React.useState("loading");
+  const [addr, setAddr] = useState(null);
 
-  React.useEffect(async ()=>{
+  React.useEffect(async () => {
     const prov = await detectEthereumProvider();
-    if(!prov){
+    if (!prov) {
       setAuthorized("not metamask");
       return;
     }
     const addr = await getAddr(prov);
+    setAddr(addr);
     const web3 = new Web3(prov);
     const mintContract = new web3.eth.Contract(contract.abi, contractAddress);
     const balance = await getBalance(mintContract, addr);
-    if(balance>0){
+    if (balance > 0) {
       setAuthorized("authorized");
-    }else{
+    } else {
       setAuthorized("not authorized");
     }
-  },[]);
+  }, []);
+
+  
 
   return (
     <div className="App">
-      <h1 style={{fontSize:35}}>Eirbmon</h1>
-      {render(authorized)}
+      <h1 style={{ fontSize: 35 }}>Eirbmon</h1>
+      {render(authorized,addr)}
       <div>
         <form action="http://localhost:8080">
-          <button type="submit" style={{padding:'10px 20px 10px 20px',fontSize:'32px',background:'linear-gradient(90deg, #FD992D 0%, #FFBF49 100%)',borderRadius:'1em', fontWeight:'bold'}}>Come back to the website</button>
+          <button type="submit" style={{ padding: '10px 20px 10px 20px', fontSize: '32px', background: 'linear-gradient(90deg, #FD992D 0%, #FFBF49 100%)', borderRadius: '1em', fontWeight: 'bold' }}>Come back to the website</button>
         </form>
       </div>
     </div>
